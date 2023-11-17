fec7f093bcc0a40bd7ea378780a32b3988341b80
loginov <loginov@greendatasoft.ru>
2023-11-17 17:23:24 +0500

    use http client

--- src/main/java/keldkemp/Main.java
+++ src/main/java/keldkemp/Main.java
@@ -2,33 +2,34 @@ package keldkemp;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    private static final HttpClient httpClient = HttpClient.newHttpClient();
+
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
         String defaultFileName = "doramaVideo.ts";
         new File(defaultFileName).createNewFile();
         String fragmentUrl = args[0];
 
         int i = 1;
         while (true) {
-            URL url = new URL(fragmentUrl.replace("{i}", String.valueOf(i)));
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            if (con.getResponseCode() != 200) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(new URI(fragmentUrl.replace("{i}", String.valueOf(i))))
+                    .build();
+            HttpResponse<byte[]> response = httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray());
+            if (response.statusCode() != 200) {
                 break;
             }
-
-            try (InputStream is = url.openStream()) {
-                Files.write(Paths.get(defaultFileName), is.readAllBytes(), StandardOpenOption.APPEND);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+            Files.write(Paths.get(defaultFileName), response.body(), StandardOpenOption.APPEND);
             i++;
         }
     }
