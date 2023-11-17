76a446368cdd58259af9f8273a8e491d92ac327b
loginov <loginov@greendatasoft.ru>
2023-11-17 17:46:43 +0500

    use http client

--- src/main/java/keldkemp/Main.java
+++ src/main/java/keldkemp/Main.java
@@ -1,36 +1,44 @@
 package keldkemp;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.time.Duration;
 
 public class Main {
-    private static final HttpClient httpClient = HttpClient.newHttpClient();
+    private static final HttpClient httpClient = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(30L))
+            .build();
 
-    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
-        String defaultFileName = "doramaVideo.ts";
-        new File(defaultFileName).createNewFile();
-        String fragmentUrl = args[0];
+    public static void main(String[] args) {
+        try {
+            String defaultFileName = "doramaVideo.ts";
+            new File(defaultFileName).createNewFile();
+            String fragmentUrl = args[0];
 
-        int i = 1;
-        while (true) {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(new URI(fragmentUrl.replace("{i}", String.valueOf(i))))
-                    .build();
-            HttpResponse<byte[]> response = httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray());
-            if (response.statusCode() != 200) {
-                break;
+            int i = 1;
+            while (true) {
+                HttpRequest request = HttpRequest.newBuilder()
+                        .GET()
+                        .uri(new URI(fragmentUrl.replace("{i}", String.valueOf(i))))
+                        .timeout(Duration.ofSeconds(30L))
+                        .build();
+                HttpResponse<byte[]> response = httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray());
+                if (response.statusCode() != 200) {
+                    break;
+                }
+                Files.write(Paths.get(defaultFileName), response.body(), StandardOpenOption.APPEND);
+                i++;
             }
-            Files.write(Paths.get(defaultFileName), response.body(), StandardOpenOption.APPEND);
-            i++;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            httpClient.close();
         }
     }
 }
